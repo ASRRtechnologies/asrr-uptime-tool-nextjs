9f549b6ac1283ef6db6f6b626a7e902ffc805f20
Rere <hesusdios@gmail.com>
2020-10-08 14:55:14 +0200

    Start of header in dashboard

--- src/components/navigation/Header.tsx
+++ src/components/navigation/Header.tsx
@@ -2,13 +2,27 @@ import React, {FunctionComponent} from 'react';
 
 interface HeaderProps {
     collapsed: boolean,
-    className?:string,
-    props?:any
+    className?: string,
+    props?: any
 }
 
 const Header: FunctionComponent<HeaderProps> = ({collapsed, className, ...props}) => {
     return (
         <div className={`header ${className}`}>
+
+            <div className="header-information">
+                <h1>Overzicht</h1>
+                <span className="header-dropdown">
+                    <p>Application</p>
+                    <select>
+                        <option>Today</option>
+                    </select>
+                </span>
+            </div>
+
+            <div>
+
+            </div>
 
 
         </div>
