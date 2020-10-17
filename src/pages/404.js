9796f2e7974697e0909e84ae05b1af6b29f3283f
Amar Ramdas <amarramdas@asrr.nl>
2020-10-17 23:11:40 +0200

    ADd 404

---
+++ src/pages/404.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import GenericError from "@/error/GenericError";
+
+function Error() {
+
+    return (
+        <>
+            <GenericError/>
+        </>
+    );
+}
+
+export default () => <Error/>
