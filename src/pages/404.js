21af0fa10d220487146f9b47c0165fbd7e65dff9
Amar Ramdas <amarramdas@asrr.nl>
2020-10-17 23:12:14 +0200

    Fix comp

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import GenericError from "@/error/GenericError";
 
 function Error() {
 
     return (
         <>
-            <GenericError/>
+            404
         </>
     );
 }
